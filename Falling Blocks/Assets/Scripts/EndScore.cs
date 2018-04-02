bc18adc440fc4910df84a496e189e4da333c4f7a
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-02 18:07:52 +0100

    Fully working game.

---
+++ Falling Blocks/Assets/Scripts/EndScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScore : MonoBehaviour {
+
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		float score = GameObject.FindObjectOfType<PlayerScore>().score;
+		text.text = "Score: " + score;
+		Destroy(GameObject.Find ("PlayerScore"));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Falling Blocks/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour {
+
+	public float score = 0f;
+
+	public Text text;
+
+	// Use this for initialization
+
+	// Update is called once per frame
+
+
+	void Start(){
+		InvokeRepeating("Scoring", 1.0f, 1.0f);
+	}
+
+	void Update () {
+		
+
+	}
+
+	void Scoring(){
+		if (GameObject.Find("Player")) {
+			score++;
+			text.text = "Score: " + score;
+		} 
+
+	}
+
+	void Awake()
+	{
+		DontDestroyOnLoad(this);
+	}
+}
