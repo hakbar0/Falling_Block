ba43ec1ddb1eafb484c733acf92535ba45f561fb
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-02 16:02:06 +0100

    Able to spawn blocks every half a second.

---
+++ Falling Blocks/Assets/Scripts/FallingBlocks.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlocks : MonoBehaviour {
+
+	public GameObject FallingBlock;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnBlock", 2.0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void SpawnBlock(){
+		GameObject instance = Instantiate (FallingBlock);
+		instance.transform.position = new Vector3(Random.Range(-7.5f, 7.5f), 5.8f, 0f);
+	}
+}
