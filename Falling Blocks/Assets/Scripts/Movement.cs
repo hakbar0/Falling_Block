26f4a108b64f1617a31edd10cf5d65a905bfa25b
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-02 14:36:49 +0100

    able to move object with arrow keys.

---
+++ Falling Blocks/Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float moveSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * moveSpeed, 0f, 0f);
+	}
+}
